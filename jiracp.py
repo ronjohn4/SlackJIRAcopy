291ff0840e2d70a9e2dbc6099466edd402e0efe7
Ron Johnson <ronjohn4@gmail.com>
2017-08-11 16:20:25 -0600

    Added in JIRA hooks

--- jiracp.py
+++ jiracp.py
@@ -1,47 +1,103 @@
 import os
 import time
 from slackclient import SlackClient
+import requests
+import json
 
 
-# starterbot's ID as an environment variable
 BOT_ID = os.environ.get("BOT_ID")
-
-# constants
-AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
-
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+AT_BOT = "<@" + BOT_ID + ">"
+
+
+def CreateIssue(project, summary, description):
+    payload = {
+        "fields": {
+            "project":
+                {
+                    "key": project
+                },
+            "summary": summary,
+            "description": description,
+            "issuetype": {
+                "name": "Story"
+            }
+        }
+    }
+    print(payload)
+    url = 'https://levelsbeyond.atlassian.net/rest/api/2/issue/'
+    response = requests.post(url, data=json.dumps(payload), auth=GetAuth(), headers={'content-type':'application/json'})
+    print(response)
+    if response.status_code == 201:
+        return True
+    return False
+
+
+def GetKey(key):
+    url = 'https://levelsbeyond.atlassian.net/rest/api/2/search?jql=key={0}'.format(key)
+    r = requests.get(url, auth=GetAuth())
+    json_return = json.loads(r.text)
+
+    if 'total' in json_return and json_return['total'] == 1:
+        return json_return['issues'][0]
+    return False
+
+
+def VerifyProject(project):
+    url = 'https://levelsbeyond.atlassian.net/rest/api/2/project/{0}'.format(project)
+    r = requests.get(url, auth=GetAuth())
+    json_return = json.loads(r.text)
+
+    if 'key' in json_return and json_return['key'] == project:
+        return True
+    return False
+
+def ParseParms(parms):
+    parmlist = parms.split()
+    if len(parmlist) == 2:
+        return parmlist[0], parmlist[1]
+    return None, None
+
+
+def GetAuth():
+    return ('rjohnson', 'Miter9le')
 
 
 def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+    key, project = ParseParms(command)
+    print(key)
+    print(project)
+
+    if key:
+        if project and VerifyProject(project):
+            sourceissue = GetKey(key)
+            if sourceissue:
+                summary = sourceissue['key'] + ' - ' + sourceissue['fields']['summary']
+                description = sourceissue['fields']['description']
+
+                if CreateIssue(project, summary, description):
+                    response = 'Key {0} duplicated into project {1}!'.format(key, project)
+                else:
+                    response = 'sorry... key {0} could NOT be duplicated into project {1}'.format(key, project)
+            else:
+                response = 'sorry... key {0} not found.'.format(key)
+        else:
+            response = 'sorry... target project {0} not found.'.format(project)
+    else:
+        response = 'sorry... I don''t understand the  request (e.g. @jiracp key project)'
+
+
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
 
 def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            print('output:', output)
             if output and 'text' in output and AT_BOT in output['text']:
-                print('output for me:', output['text'].split(AT_BOT)[1].strip().lower())
-                print('channel:', output['channel'])
                 # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                return output['text'].split(AT_BOT)[1].strip().upper(), \
                        output['channel']
     return None, None
 
